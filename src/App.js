883a9bf96e6478c0f16ee3d32735d7d1c9c423c7
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-05-19 15:57:53 +0530

    Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { Box } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Toolbar from "./components/Toolbar";
+import Sidebar from "./components/Sidebar";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Box height={"100vh"} display={"grid"} gridTemplateColumns={"95% 5%"}>
+      <Box display={"grid"} gridTemplateColumns={"95% 5%"}>
         <Box>
           <Toolbar />
         </Box>
-        <Box bgColor={"green.900"}>Right</Box>
+        <Box>
+          <Sidebar />
+        </Box>
       </Box>
     </div>
   );
